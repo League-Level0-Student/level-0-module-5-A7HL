2a98707ca30b405b706e9f364702bca8bf54e3d2
TJHL <tjhl8545@gmail.com>
2022-02-22 19:59:30 -0800

    Hi Again

--- src/_01_algorithms/_1_prime_or_not/PrimeOrNot.java
+++ src/_01_algorithms/_1_prime_or_not/PrimeOrNot.java
@@ -1,10 +1,13 @@
 package _01_algorithms._1_prime_or_not;
 
+import javax.swing.JOptionPane;
+
 public class PrimeOrNot {
 	public static void main(String[] args) {
+		String num = JOptionPane.showInputDialog("Enter a number");
+		for(int a = 0; a < 10; a +=1) {
+		int numnum = Integer.parseInt(num);
 		
-		
-		
+		}
 	}
-	
 }
