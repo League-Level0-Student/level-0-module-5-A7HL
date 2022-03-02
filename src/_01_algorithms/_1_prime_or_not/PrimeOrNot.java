444103bae550668280d65b715fe599ce26f2775b
TJHL <tjhl8545@gmail.com>
2022-03-01 19:56:54 -0800

    hi : )

--- src/_01_algorithms/_1_prime_or_not/PrimeOrNot.java
+++ src/_01_algorithms/_1_prime_or_not/PrimeOrNot.java
@@ -5,9 +5,17 @@ import javax.swing.JOptionPane;
 public class PrimeOrNot {
 	public static void main(String[] args) {
 		String num = JOptionPane.showInputDialog("Enter a number");
-		for(int a = 0; a < 10; a +=1) {
+		boolean prime = false;
 		int numnum = Integer.parseInt(num);
-		
+		for(int a = 2; a <= numnum /2; ++a) {
+		if(numnum%a==0){
+		prime = true;
+		break;
+			}
 		}
+		if (!prime)
+			System.out.println(numnum + " is a prime number");
+		else
+			System.out.println(numnum + " is not a prime number");
 	}
 }
